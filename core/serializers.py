3d283b003940e6ad3a00d9bf18b77726317295de
Alban Tiberghien <atiberghien@numnprod.fr>
2023-04-05 09:01:26 +0200

    Convert all serializers to flex field serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,40 +1,50 @@
 from rest_framework import serializers
+from taggit.serializers import (TagListSerializerField, TaggitSerializer)
+from rest_flex_fields import FlexFieldsModelSerializer
 from .models import *
 
-class DigitalServiceSerializer(serializers.ModelSerializer):
+class ZoneSerializer(FlexFieldsModelSerializer):
+    class Meta:
+        model = Zone
+        fields = "__all__"
+
+
+class DigitalServiceSerializer(FlexFieldsModelSerializer):
     class Meta:
         model = DigitalService
-        # fields = "__all__"
-        exclude = ('uses',)
-        
-        
-class DigitalUseSerializer(serializers.ModelSerializer):
-    services = DigitalServiceSerializer(many=True, read_only=True)
+        fields = "__all__"
+        expandable_fields = {
+            'uses': ('core.DigitalUseSerializer', {'many': True, 'read_only':True}),
+            'zone': ZoneSerializer,
+        }
+              
+
+class DigitalUseSerializer(TaggitSerializer, FlexFieldsModelSerializer):
+    tags = TagListSerializerField()
     class Meta:
         model = DigitalUse
-        # fields = "__all__"
-        exclude = ('items',)
+        fields = ('id', 'title', 'slug', 'description', 'services', 'items', 'tags')
+        expandable_fields = {
+            'services': (DigitalServiceSerializer, {'many': True, 'read_only':True}),
+            'items': ('core.ItemSerializer', {'many': True, 'read_only':True}),
+        }
 
+class ItemSerializer(FlexFieldsModelSerializer):
 
-class NestedDigitalUseSerializer(serializers.ModelSerializer):
-    services = DigitalServiceSerializer(many=True, read_only=True)
-    class Meta:
-        model = DigitalUse
-        # fields = "__all__"
-        exclude = ('items',)
-
-class ItemSerializer(serializers.ModelSerializer):
-    uses = NestedDigitalUseSerializer(many=True, read_only=True)
-    
     class Meta:
         model = Item
-        # fields = "__all__"
-        exclude = ('room',)
+        fields = "__all__"
+        expandable_fields = {
+          'uses': (DigitalUseSerializer, {'many': True, 'read_only':True}),
+          'room': ('core.RoomSerializer', {'many': False, 'read_only':True}),
+        }
 
 
-class RoomSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, read_only=True)
+class RoomSerializer(FlexFieldsModelSerializer):
     
     class Meta:
         model = Room
-        fields = "__all__"
+        fields = ('id', 'name', 'slug', 'description', 'video', 'items')
+        expandable_fields = {
+            'items': (ItemSerializer, {'many': True, 'read_only':True}), 
+        }
