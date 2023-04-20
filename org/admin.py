15262b672e143bc3351ce3e58a1b5649d8f6c6cc
Alban Tiberghien <atiberghien@numnprod.fr>
2023-04-20 09:36:39 +0200

    Customize admin

--- org/admin.py
+++ org/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from rest_framework_api_key.models import APIKey
 from rest_framework_api_key.admin import APIKeyModelAdmin
 from rest_framework.authtoken.models import TokenProxy
-from .models import OrganizationAPIKey, Organization
+from allauth.socialaccount.models import SocialApp, SocialToken, SocialAccount
+from .models import OrganizationAPIKey, Organization, User
 
-from .models import User
 admin.site.unregister(TokenProxy)
 admin.site.unregister(APIKey)
 admin.site.unregister(Group)
-admin.site.register(User, UserAdmin)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
+admin.site.unregister(SocialAccount)
 
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = [s for s in BaseUserAdmin.fieldsets]
+    fieldsets.append(('Organization', {'fields': ('organization',)}))
 
 @admin.register(Organization)
 class OrganizationAdmin(admin.ModelAdmin):
