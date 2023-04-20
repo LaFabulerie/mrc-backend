9fd5c1db8be6a1cb8bd54cafe8c72e4da0694eac
Alban Tiberghien <atiberghien@numnprod.fr>
2023-04-20 09:33:30 +0200

    Set default api permission

--- mrc/settings.py
+++ mrc/settings.py
@@ -117,8 +117,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        'rest_framework.permissions.IsAuthenticated'
-        # "rest_framework_api_key.permissions.HasAPIKey",
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
