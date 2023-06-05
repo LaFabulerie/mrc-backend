819ee0c8765bf712267b7c23b3e5a22eca553854
Alban Tiberghien <atiberghien@numnprod.fr>
2023-06-05 16:03:05 +0200

    Add main color field to Room model

--- core/models.py
+++ core/models.py
@@ -12,6 +12,7 @@ class Room(models.Model):
     slug = AutoSlugField(populate_from='name', unique=True)
     description = models.TextField(blank=True, null=True)
     video = models.FileField(upload_to=room_video_path, blank=True, null=True)
+    main_color = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.name
