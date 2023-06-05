e81c69ac881b619106b4b66d2c5c7807689387ce
Alban Tiberghien <atiberghien@numnprod.fr>
2023-06-05 16:01:16 +0200

    Configure media/static urls

--- mrc/urls.py
+++ mrc/urls.py
@@ -1,5 +1,7 @@
+import stat
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -30,6 +32,10 @@ urlpatterns = [
     re_path(r'^api/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 print(f"\n{10*'*'} ENVIRONMENT : {settings.ENV_NAME} {10*'*'}\n")
