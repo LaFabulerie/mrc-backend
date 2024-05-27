3fe4f1a735352f253231750acdbd9c0e0e54360f
Mindiell <github@mindiell.net>
2024-05-27 13:09:24 +0200

    fix: accept external API GET requests

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from core.permissions import IsLocalAccess
 from org.permissions import HasOrganizationAPIKey
 
@@ -9,20 +9,20 @@ from .serializers import FeedbackSerializer, QuestionSerializer, AnswerSerialize
 class FeedbackViewSet(viewsets.ModelViewSet):
     serializer_class = FeedbackSerializer
     queryset = Feedback.objects.all()
-    permission_classes = [IsLocalAccess | HasOrganizationAPIKey | IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class AnswerChoiceViewset(viewsets.ModelViewSet):
     serializer_class = AnswerChoiceSerializer
     queryset = AnswerChoice.objects.all()
-    permission_classes = [IsLocalAccess | HasOrganizationAPIKey | IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class QuestionViewset(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     queryset = Question.objects.all()
-    permission_classes = [IsLocalAccess | HasOrganizationAPIKey | IsAuthenticated]
+    permission_classes = [AllowAny]
 
 class AnswerViewSet(viewsets.ModelViewSet):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
-    permission_classes = [IsLocalAccess | HasOrganizationAPIKey | IsAuthenticated]
+    permission_classes = [AllowAny]
